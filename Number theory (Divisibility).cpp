c49a5c04eefdaed9e89c4ca055ebc5475c6afd58
raihan02 <raihanraju02@gmail.com>
2016-11-08 21:32:08 +0600

    Create Number theory (Divisibility).cpp

---
+++ Number theory (Divisibility).cpp
@@ -0,0 +1,12 @@
+Divisibility:
+Rules for Zero: Divisible by all integer, (expect zero)
+Rules for Two : If last digit must Divisible by 2 (ex: 12)
+Rules for Four: If last two digits divisible by 4(ex 1231224) last 2 digits are 24 (24 % 4 == 0)
+Rules for Eight: If last 3 digits is divisible by 8
+Rules for three: If sum of digit is divisible by 3. (ex 123) , 1+2+3 = 6 (6 % 3 = 0)
+Rules for Nine:  If sum of digit is divisible by 9.
+Rules for Five: If last digit is 0 or 5.
+Rules for 6: if the number is divisible by 2 & 3
+Rules for Twelve: If the number is divisible by 3 & 4.
+Rules for Eighteen: 18 = 2 * 9 check (last digit is divisible 2 and sum of digits is divisible by nine)
+Rules for Thirty Six : 36 = (4 * 9)  check last two digits are divisible by 4 and sum of digits is divisible by nine
