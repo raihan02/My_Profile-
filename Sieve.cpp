85aa55650b58dd7bc27658273f54f62e3bca2733
raihan02 <raihanraju02@gmail.com>
2016-11-14 23:54:24 +0600

    Create Sieve.cpp

---
+++ Sieve.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int prime[3000000] , nPrime;
+int mark[10000002];
+void sieve(int n)
+{
+    int i , j , limit = sqrt(n * 1.) + 2;
+    mark[1] = 1;
+    for(int i = 4; i <= n; i += 2)
+        mark[i] = 1;
+    prime[nPrime++] = 2;
+
+    for(i = 3; i <= n; i++)
+    {
+        if(!mark[i])
+        {
+            prime[nPrime++] = i;
+            if(i <= limit)
+            {
+                for(j = i * i; j <= n; j+= i * 2)
+                {
+                    mark[j] = 1;
+                }
+            }
+        }
+    }
+}
+int main()
+{
+
+
+
+}
