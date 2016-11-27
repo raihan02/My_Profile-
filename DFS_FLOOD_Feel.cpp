bc06a38c705437a91d74db38fb95b4bec7c03730
raihan02 <raihanraju02@gmail.com>
2016-11-27 22:03:19 +0600

    Create DFS_FLOOD_Feel.cpp

---
+++ DFS_FLOOD_Feel.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; /// move up down && corner /// If we want only move in up and down , thne 
+int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; /// UVA problem samle (Oil deposite )
+int row , col;
+char maze[101][101];
+int r[101][101];
+void dfs (int x, int y)
+{
+    r[x][y] = 1;
+
+    for(int i = 0; i < 8; i++)
+    {
+        int a, b;
+        a = x + fx[i];
+        b = y + fy[i];
+
+        if( a >= 0 && a < row && b >= 0 && b < col && r[a][b] == 0)
+        {
+            r[a][b] = 1;
+            if(maze[a][b] == '@')
+            {
+                dfs(a , b);
+            }
+        }
+    }
+}
+int main()
+{
+    while(cin >> row >> col)
+    {
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                cin >> maze[i][j];
+            }
+        }
+        memset(r , 0, sizeof r);
+        int cnt = 0;
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                if(r[i][j] == 0)
+                {
+                    if(maze[i][j] == '@')
+                    {
+                        dfs(i , j);
+                        cnt++;
+                    }
+                }
+            }
+        }
+        cout<< cnt << endl;
+    }
+}
