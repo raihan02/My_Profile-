534a5e2f8cb1f2656a41668c35732da4850846f7
raihan02 <raihanraju02@gmail.com>
2017-03-24 12:34:44 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,86 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-#define ll          long long int
-#define sf          scanf
-#define pf          printf
-#define F           first
-#define S           second
-#define pb          push_back
-#define GCD(a, b)   __gcd(a, b)
-#define LCM(a, b)   ((a*b)/GCD(a, b))
-
-#define MOD         1000000007
-#define M           10000
-#define deb(x) cerr << #x << " = " << x << endl;
-template < typename F, typename S >
-ostream& operator << ( ostream& os, const pair< F, S > & p )
+#define mem(x,y) memset(x, y, sizeof(x))
+#define inf 1<<28
+#define pb push_back
+#define fin(prob) freopen("prob.in", "r", stdin)
+#define fout(prob) freopen("prob.out", "w", stdout)
+#define si(a) int a; scanf("%d", &a);
+#define sll(a) ll a; scanf("%lld", &a);
+#define sllu(a) unsigned ll a; scanf("%llu", &a);
+#define sd(a) double a; scanf("%lf", &a);
+#define deb(x) cerr<< #x <<"="<<x<<endl;
+const double pi=2*acos(0);
+typedef string st;
+typedef vector <int> vi;
+typedef vector <string> vs;
+typedef pair <int, int> ii;
+typedef long long ll;
+typedef vector < pair <int, int> > vii;
+typedef priority_queue<int,vector<int>,greater<int> > min_heap;
+bool pali(string temp, int i, int j)
 {
-    return os << "(" << p.first << ", " << p.second << ")";
+    if(i>=j) return true;
+    if(temp[i]==temp[j]) return pali(temp, i+1, j-1);
+    return false;
 }
-
-template < typename T >
-ostream &operator << ( ostream & os, const vector< T > &v )
+template <class T> inline T gcd(T a, T b)
 {
-    os << "{";
-    typename vector< T > :: const_iterator it;
-    for( it = v.begin(); it != v.end(); it++ )
-    {
-        if( it != v.begin() ) os << ", ";
-        os << *it;
-    }
-    return os << "}";
+    if(a<0) return gcd(-a, b);
+    if(b<0) return gcd(a,-b);
+    return (b==0) ? a : gcd(b, a%b);
 }
-
-template < typename T >
-ostream &operator << ( ostream & os, const set< T > &v )
+template <typename F, typename S> ostream &operator << (ostream & os, const map<F,S>&v)
 {
-    os << "[";
-    typename set< T > :: const_iterator it;
-    for ( it = v.begin(); it != v.end(); it++ )
+    os<<"[";
+    typename map<F,S>::const_iterator it;
+
+    for(it=v.begin(); it!=v.end(); it++)
     {
-        if( it != v.begin() ) os << ", ";
-        os << *it;
+        if(it != v.begin()) os<<",";
+        os<< it ->first<<"="<<it->second;
     }
-    return os << "]";
+    return os<<"]";
 }
-
-template < typename F, typename S >
-ostream &operator << ( ostream & os, const map< F, S > &v )
+int main()
 {
-    os << "[";
-    typename map< F , S >::const_iterator it;
-    for( it = v.begin(); it != v.end(); it++ )
-    {
-        if( it != v.begin() ) os << ", ";
-        os << it -> first << " = " << it -> second ;
-    }
-    return os << "]";
-}
-
-
-#define deb(x) cerr << #x << " = " << x << endl;
-#define visited 1
-#define unvisited -1
-typedef pair<int ,int> pi;
-typedef vector <pi> vc;
-int arr[100005];
-vc node[100005];
-priority_queue <pi> pq;
-
-template<class T>
-
-inline bool fs(T &x)
-{
-    int c=getchar();
-    int sgn=1;
-    while(~c&&c<'0'||c>'9')
-    {
-        if(c=='-')sgn=-1;
-        c=getchar();
-    }
-    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())
-        x=x*10+c-'0';
-    x*=sgn;
-    return ~c;
+    
 }
