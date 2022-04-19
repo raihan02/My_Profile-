f8e0e698c6a5d2efa53a3a87aa843de52b705d53
Raihan <raihanraju02@gmail.com>
2022-04-19 13:12:37 +0600

    Create Monotonic_Queue.cpp

---
+++ Monotonic_Queue.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        vector<int> res;
+        
+        deque <int> dq;
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            if(dq.size() > 0 && i - k == dq.front()){
+                dq.pop_front();
+            }
+            
+            while(dq.size() > 0 && nums[dq.back()] < nums[i] ){
+                dq.pop_back();
+            }
+            
+            dq.push_back(i);
+            
+            if(i >= k - 1){
+                res.push_back(nums[dq.front()]);
+            }
+        }
+        
+        return res;
+    }
+};
